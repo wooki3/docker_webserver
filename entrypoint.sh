597bac979609080efb64cff05679dcb5919871f4
wooki3 <94880382+wooki3@users.noreply.github.com>
2022-07-29 17:32:18 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ yes n | cp -iR /var/www/. /data/www/
 yes n | cp -iR /etc/php/. /data/config/php
 rm /first.run
 chown -R www-data:www-data /data/www
-debconf-set-selections <<< "postfix postfix/mailname string admin@brandonsmith.xyz"
-debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+#debconf-set-selections <<< "postfix postfix/mailname string admin@brandonsmith.xyz"
+debconf-set-selections << "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install --assume-yes postfix\nelse
 cp -fR /data/config/apache/. /etc/apache2
 cp -fR /data/config/php/. /etc/php
